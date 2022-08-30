7923683d2da25ceb86609c6f4d6ea90853319968
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-08-30 17:43:12 -0300

    feat: add password input
    Co-authored-by: mateusfpleite <mateusfpleite@users.noreply.github.com>

--- big-project/pages/index.jsx
+++ big-project/pages/index.jsx
@@ -1,32 +1,36 @@
 import useGenericState from "../../hooks/useGenericState";
+import Button from '@mui/material/Button';
 
 function Login() {
   const INITIAL_STATE = {
     emailInput: '',
     passwordInput: '',
   }
-  const [genericState, setGenericState] = useGenericState(INITIAL_STATE);
+  const [ genericState, setGenericState ] = useGenericState(INITIAL_STATE);
 
   return (
-    <div>
+    <form onSubmit>
       <label htmlFor="emailInput">
         Email:
         <input
           name="emailInput"
           id="emailInput"
           type="text"
-          onChange={ setGenericState }
-          value={ genericState.emailInput }
-        />
-        <input 
-        type="password"
-        name="passwordInput"
-        id="passwordInput"
-        onChange={ setGenericState }
-        value={ genericState.passwordInput }
+          onChange={setGenericState}
+          value={genericState.emailInput}
         />
       </label>
-    </div>
+      <label htmlFor="passwordInput">
+        Password:
+        <input
+          type="password"
+          name="passwordInput"
+          id="passwordInput"
+          onChange={setGenericState}
+          value={genericState.passwordInput}
+        />
+      </label>
+    </form>
   )
 }
 
