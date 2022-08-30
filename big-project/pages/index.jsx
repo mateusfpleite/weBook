52808da9ca28f82fc9537f53a0d562325532d088
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-08-30 17:13:04 -0300

    starting the project

---
+++ big-project/pages/index.jsx
@@ -0,0 +1,16 @@
+function Login() {
+  
+  return (
+    <div>
+      <label htmlFor="emailInput">
+        Email:
+        <input
+          id="emailInput"
+          type="text"
+        />
+      </label>
+    </div>
+  )
+}
+
+export default Login;
