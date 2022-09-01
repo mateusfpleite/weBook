fccdc6dfd2422c6e05ef36a22bb40a2d3f4ccb6e
Mateus <mateusfpleite@gmail.com>
2022-09-01 17:27:56 -0300

    feat: completes SearchBar v1.0
    Co-authored-by: Leonardo Guimarães Urci <Leonardoozx@users.noreply.github.com>

--- big-project/components/SearchBar.jsx
+++ big-project/components/SearchBar.jsx
@@ -1,18 +1,23 @@
 import useGenericState from '../hooks/useGenericState';
 import searchBarFetch from '../utils/searchBarFetch';
-import Link from 'next/link';
 import { useState } from 'react';
+import SearchBarCard from './SearchBarCard';
+import Link from 'next/link';
 
 function SearchBar() {
   const INITIAL_STATE = { searchBar: '' };
   const [genericState, setGenericState] = useGenericState(INITIAL_STATE);
-  const [state, setState] = useState([]);
+  const [searchBarData, setSearchBarData] = useState([]);
 
+  const verifySearchBarText = (text) => {
+    const reg = /.*\S.*/;
+    return reg.test(text);
+  };
 
   const searchBarRequest = async (event) => {
     const { target } = event;
     setGenericState(event)
-    setState(await searchBarFetch(target.value) || []);
+    setSearchBarData(await searchBarFetch(target.value) || []);
   };
 
   return (
@@ -23,26 +28,24 @@ function SearchBar() {
         onChange={(e) => searchBarRequest(e)}
         value={genericState.searchBar}
       />
-      <div style={{ position: 'absolute' }}>
-        {state.length
-          ? (state.map(({ id, volumeInfo }) => (
-            <>
-              <Link href={`/book/${id}`} key={id}>
-                <a>
-                  <div style={{ position: 'relative', backgroundColor: 'white', border: '1px solid black' }}>
-                    <img
-                      src={volumeInfo.imageLinks?.thumbnail || 'https://tinyurl.com/36ys7v4w'}
-                      style={{ width: '40px' }}
-                    />
-                    <span>{volumeInfo.title}</span>
-                  </div>
-                </a>
-              </Link>
-            </>
-          )))
-          : 'No results found'}
-      </div>
-
+      {verifySearchBarText(genericState.searchBar) &&
+        <div style={{ position: 'absolute' }}>
+          {searchBarData.length
+            ? (
+              <>
+                {searchBarData.map((data) => (
+                  <SearchBarCard
+                    {...data}
+                    query={genericState.searchBar}
+                  />
+                ))}
+                <Link href={`/book/allVolumes/${genericState.searchBar}`}>See all!</Link>
+              </>
+            )
+            : 'No results found'
+          }
+        </div>
+      }
     </>
   )
 }
---
+++ big-project/components/SearchBarCard.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+function SearchBarCard({ id, volumeInfo }, query) {
+  console.log(query)
+  return (
+    <>
+      <Link href={`/book/${id}`} key={id}>
+        <a>
+          <div style={{ position: 'relative', backgroundColor: 'white', border: '1px solid black' }}>
+            <img
+              src={volumeInfo.imageLinks?.thumbnail || 'https://tinyurl.com/36ys7v4w'}
+              style={{ width: '40px' }}
+            />
+            <span>{volumeInfo.title}</span>
+          </div>
+        </a>
+      </Link>
+    </>
+  )
+}
+
+export default SearchBarCard;
