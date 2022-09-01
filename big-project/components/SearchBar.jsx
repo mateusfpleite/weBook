bcabb6d96a5ae6c764b3c1c42c74b3be5c1459fa
Leonardo Guimarães Urci <Leonardoozx@users.noreply.github.com>
2022-09-01 16:56:49 -0300

    feat: implementing searchBar design and functionality
    Co-authored-by: Leonardo Guimarães Urci <Leonardoozx@users.noreply.github.com>

--- big-project/components/SearchBar.jsx
+++ big-project/components/SearchBar.jsx
@@ -1,31 +1,48 @@
 import useGenericState from '../hooks/useGenericState';
+import searchBarFetch from '../utils/searchBarFetch';
+import Link from 'next/link';
+import { useState } from 'react';
 
 function SearchBar() {
   const INITIAL_STATE = { searchBar: '' };
   const [genericState, setGenericState] = useGenericState(INITIAL_STATE);
+  const [state, setState] = useState([]);
+
 
   const searchBarRequest = async (event) => {
     const { target } = event;
     setGenericState(event)
-    
+    setState(await searchBarFetch(target.value) || []);
   };
 
   return (
     <>
       <input
-      list="searchBar"
-      placeholder="Search..."
-      name="searchBar"
-      onChange={ (e) => searchBarRequest(e) }
-      value={ genericState.searchBar }
-    />
-      <datalist id="searchBar">
-        <option value="Edge" />
-        <option value="Firefox" />
-        <option value="Chrome" />
-        <option value="Opera" />
-        <option value="Safari" />
-      </datalist>
+        placeholder="Search..."
+        name="searchBar"
+        onChange={(e) => searchBarRequest(e)}
+        value={genericState.searchBar}
+      />
+      <div style={{ position: 'absolute' }}>
+        {state.length
+          ? (state.map(({ id, volumeInfo }) => (
+            <>
+              <Link href={`/book/${id}`} key={id}>
+                <a>
+                  <div style={{ position: 'relative', backgroundColor: 'white', border: '1px solid black' }}>
+                    <img
+                      src={volumeInfo.imageLinks?.thumbnail || 'https://tinyurl.com/36ys7v4w'}
+                      style={{ width: '40px' }}
+                    />
+                    <span>{volumeInfo.title}</span>
+                  </div>
+                </a>
+              </Link>
+            </>
+          )))
+          : 'No results found'}
+      </div>
+
     </>
   )
 }
--- big-project/utils/searchBarFetch.js
+++ big-project/utils/searchBarFetch.js
@@ -1,5 +1,9 @@
-const searchBarFetch = async () => {
-  
+import axios from 'axios';
+
+const searchBarFetch = async (value) => {
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=%22${value}%22&maxResults=5`;
+  const { data: { items } } = await axios.get(URL);
+  return items;
 }
 
 export default searchBarFetch;
