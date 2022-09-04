211764cff8044e8cb6c25a5af7cabffabf2c61e1
Mateus <mateusfpleite@gmail.com>
2022-09-04 19:00:26 -0300

    feat: working on filters. Created categories list and conditionally rendering it
    Co-authored-by: Leonardo Guimarães Urci <Leonardoozx@users.noreply.github.com>

--- pages/books/allVolumes/[volumeName].jsx
+++ pages/books/allVolumes/[volumeName].jsx
@@ -1,19 +1,35 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import BookCard from '../../../components/BookCard/BookCard';
 import searchBarFetch from "../../../utils/searchBarFetch";
 
 function AllVolumes({ data }) {
+  const [showFiltersList, setListItem] = useState({});
   const router = useRouter();
 
   if (router.isFallback) return (<h1>Loading...</h1>)
-
+  const categories = [...new Set(data.map((book) => (
+    book.volumeInfo.categories?.join(', ')
+  )))];
   return (
+    <div style={{display: "flex"}}>
+      <div>
+        <h2>Filters:</h2> 
+        <ul style={{listStyleType: 'none'}}>
+        <h4 name="category" 
+        onClick={() => setListItem({category: 'category'})}
+        style={{cursor: 'pointer' }}>Category</h4>
+        {showFiltersList.category 
+        && categories.map((category) => <li style={{marginBottom: '5px'}}>{category}</li>)}
+        </ul>
+      </div>
     <div>
       {data.map((book) => (
         <div key={book.id}>
           <BookCard book={book} />
         </div>
       ))}
+    </div>
     </div>
   )
 }
