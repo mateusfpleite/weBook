06387ddf8edac070072b0bad7278a23461672002
Mateus <mateusfpleite@gmail.com>
2022-09-04 19:15:24 -0300

    fix: correcting bug that was not allowing "categories" (and subsequent filters) list to collapse once they've been expanded
    Co-authored-by: Leonardo Guimarães Urci <Leonardoozx@users.noreply.github.com>

--- pages/books/allVolumes/[volumeName].jsx
+++ pages/books/allVolumes/[volumeName].jsx
@@ -4,22 +4,27 @@ import BookCard from '../../../components/BookCard/BookCard';
 import searchBarFetch from "../../../utils/searchBarFetch";
 
 function AllVolumes({ data }) {
-  if (router.isFallback) return (<h1>Loading...</h1>)
-  
-  const [showFiltersList, setListItem] = useState({});
   const router = useRouter();
+  if (router.isFallback) return (<h1>Loading...</h1>)
+
+  const [showFiltersList, setListItem] = useState({});
 
   const categories = [...new Set(data.map((book) => (
     book.volumeInfo.categories?.join(', ')
   )))];
+
+  const showFilterItem = ({target}) => {
+    setListItem((prev) => ({...prev, [target.id]: !prev[target.id]}))
+  }
+  console.log(showFiltersList);
   return (
     <div style={{display: "flex"}}>
       <div>
         <h2>Filters:</h2> 
         <ul style={{listStyleType: 'none'}}>
-        <h4 name="category" 
-        onClick={() => setListItem({category: 'category'})}
-        style={{cursor: 'pointer' }}>Category</h4>
+        <h4 id="category" 
+        onClick={(event) => showFilterItem(event)}
+        style={{cursor: 'pointer' }}>Categories</h4>
         {showFiltersList.category 
         && categories.map((category) => <li style={{marginBottom: '5px'}}>{category}</li>)}
         </ul>
