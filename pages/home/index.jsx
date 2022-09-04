8493773846e6ce1da9b28ee4d42f6ac0c804e0e3
Mateus <mateusfpleite@gmail.com>
2022-09-04 00:45:44 -0300

    feat: installing and testing react-facebook-login library to allow login with facebook
    Co-authored-by: Leonardo Guimarães Urci <Leonardoozx@users.noreply.github.com>

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,11 +1,21 @@
 import axios from 'axios';
 // import BookCard from '../../components/BookCard/BookCard';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import FacebookLogin from 'react-facebook-login';
 function Home() {
+ function responseFacebook (response) {
+  console.log(response)
+ }
   return (
     <>
       <h1>You are in the home page!</h1>
       <SearchBar />
+      <FacebookLogin
+    appId="1250954195759976"
+    autoLoad={true}
+    fields="name,email,picture"
+    callback={responseFacebook}
+   />,
       
     </>
   )
