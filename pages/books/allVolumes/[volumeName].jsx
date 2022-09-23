0fc70ea66af3eeec57fcd8f25c3d19736d4511ec
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-23 14:14:49 -0300

    feat: starts making the pages from showAll page
    Co-authored-by: mateusfpleite <mateusfpleite@users.noreply.github.com>

--- pages/books/allVolumes/[volumeName].jsx
+++ pages/books/allVolumes/[volumeName].jsx
@@ -1,78 +1,109 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCard from '../../../components/BookCard/BookCard';
-import searchBarFetch from "../../../utils/searchBarFetch";
+import searchBarFetch from '../../../utils/searchBarFetch';
+
+let categoryKey = -1250;
+let authorKey = 1250;
 
 function AllVolumes({ data }) {
   const router = useRouter();
-
-  const [ showFiltersList, setFiltersList ] = useState({ category: false, authors: false });
-  if (router.isFallback) return (<h1>Loading...</h1>)
-
-  let categoryKey = 0;
-  let authorKey = 0;
-
-  const categories = [ ...new Set(data.map((book) => (
-    book.volumeInfo.categories?.join(', ')
-  ))) ];
-  const authors = [ ...new Set(data.map((book) => (
-    book.volumeInfo.authors?.join(', ')
-  ))) ];
-
+  const { volumeName } = router.query;
+  const [showFiltersList, setFiltersList] = useState({
+    category: false,
+    authors: false,
+  });
+  const [activeFilters, setFilter] = useState({});
+  const [filteredData, setFilteredData] = useState([]);
+  useEffect(() => {
+    const applyFilters = async () => {
+      const fetch =
+        activeFilters.type === 'category'
+          ? await searchBarFetch(volumeName, activeFilters.value)
+          : '';
+      console.log(fetch);
+      setFilteredData(fetch);
+    };
+    applyFilters();
+  }, [activeFilters]);
+  if (router.isFallback) return <h1>Loading...</h1>;
+  const categories = [
+    ...new Set(data.map((book) => book.volumeInfo.categories?.join(', '))),
+  ];
+  const authors = [
+    ...new Set(data.map((book) => book.volumeInfo.authors?.join(', '))),
+  ];
   const showFilters = ({ target: { id } }) => {
-    setFiltersList((prevState) => ({ ...prevState, [ id ]: !prevState[ id ] }))
+    setFiltersList((prevState) => ({ ...prevState, [id]: !prevState[id] }));
   };
 
+  console.log(activeFilters);
+
+  let books = activeFilters.type ? filteredData || [] : data;
+
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: 'flex' }}>
       <div>
         <h2>Filters:</h2>
         <ul style={{ listStyleType: 'none' }}>
-          <h4
-            id="category"
-            onClick={showFilters}
-            style={{ cursor: 'pointer' }}
-          >
+          <h4 id="category" onClick={showFilters} style={{ cursor: 'pointer' }}>
             Category
           </h4>
-          {showFiltersList.category && categories.map((category) => {
-            categoryKey += 1;
-            return <li key={categoryKey} style={{ marginBottom: '5px' }}>{category}</li>
-          })}
-          <h4
-            id="authors"
-            onClick={showFilters}
-            style={{ cursor: 'pointer' }}
-          >
+          {showFiltersList.category &&
+            categories.map((category) => {
+              categoryKey += 1;
+              return (
+                <li
+                  key={categoryKey}
+                  style={{ marginBottom: '5px' }}
+                  onClick={() =>
+                    setFilter({ type: 'category', value: category })
+                  }
+                >
+                  {category}
+                </li>
+              );
+            })}
+          <h4 id="authors" onClick={showFilters} style={{ cursor: 'pointer' }}>
             Authors
           </h4>
-          {showFiltersList.authors && authors.map((author) => {
-            authorKey += 1;
-            return <li key={authorKey} style={{ marginBottom: '5px' }}>{author}</li>
-          })}
+          {showFiltersList.authors &&
+            authors.map((author) => {
+              authorKey += 1;
+              return (
+                <li
+                  key={authorKey}
+                  style={{ marginBottom: '5px' }}
+                  onClick={() => setFilter({ type: 'author', value: author })}
+                >
+                  {author}
+                </li>
+              );
+            })}
         </ul>
       </div>
       <div>
-        {data.map((book) => (
+        {books.map((book) => (
           <div key={book.id}>
             <BookCard book={book} />
           </div>
         ))}
       </div>
+      <button type="button">Next page</button>
     </div>
-  )
+  );
 }
 
 export async function getStaticPaths() {
   return {
-    paths: [ { params: { volumeName: 'Harry' } } ],
-    fallback: true
-  }
+    paths: [{ params: { volumeName: 'Harry' } }],
+    fallback: true,
+  };
 }
 
 export async function getStaticProps(context) {
   const { volumeName } = context.params;
-  const data = await searchBarFetch(volumeName, false)
+  const data = await searchBarFetch(volumeName, false);
   return {
     props: {
       data,
--- utils/searchBarFetch.js
+++ utils/searchBarFetch.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const searchBarFetch = async (value, maxResults, language) => {
+const searchBarFetch = async (value, category, maxResults, language) => {
   const lang = language ? `&langRestrict=${language}` : '';
-  const hasMaxResults = maxResults ? '&maxResults=5' : '&maxResults=10';
-  const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}${hasMaxResults}${lang}`;
+  const hasMaxResults = maxResults ? `&maxResults=$${maxResults}` : '&maxResults=40';
+  const withCat = category ? `+subject:${category}`: '&';
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}${withCat}${hasMaxResults}${lang}`;
   console.log(URL);
   const {
     data: { items },
