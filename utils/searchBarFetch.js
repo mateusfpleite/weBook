b0860a807cc7080f3c251159cf9b6c071703dde7
Mateus <mateusfpleite@gmail.com>
2022-10-11 14:37:55 -0300

    fix: fixing bug in searchBarFetch

--- utils/searchBarFetch.js
+++ utils/searchBarFetch.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
-const searchBarFetch = async ({value, category=null, maxResults=null, language=null, author=null}) => {
+const searchBarFetch = async (
+  {value, category=null, maxResults=null, language=null, author=null, startIndex=null}) => {
   const lang = language ? `&langRestrict=${language}` : '';
-  const hasMaxResults = maxResults ? `&maxResults=$${maxResults}` : '&maxResults=20';
-  const withCat = category ? `+subject:'${category}'`: '&';
-  const withAuthor = author ? `+inauthor:'${author}'` : '&'
-  const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}${withAuthor}${withCat}${hasMaxResults}${lang}`;
+  const hasMaxResults = maxResults ? `&maxResults=${maxResults}` : '&maxResults=20';
+  const withCat = category ? `+subject:'${category}'`: '';
+  const withAuthor = author ? `+inauthor:'${author}'` : '';
+  const withIndex = startIndex ? `&startIndex=${startIndex}` : '';
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}${withIndex}${withAuthor}${withCat}${hasMaxResults}${lang}`;
+  console.log(URL);
   const {
     data: { items },
   } = await axios.get(URL);
