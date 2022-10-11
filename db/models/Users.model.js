0933c5ca8d05a892df1b554e6d0d7712dac1938d
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-10 21:43:54 -0300

    feat: user model

---
+++ db/models/Users.model.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      profilePicture: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users',
+    }
+  );
+  return User;
+};
