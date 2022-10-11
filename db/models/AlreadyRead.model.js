154b94b2c61ea63ad54f52ab55d7b9153aa874e0
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-11 20:07:33 -0300

    starts the N:N to other models

--- db/models/AlreadyRead.model.js
+++ db/models/AlreadyRead.model.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   AlreadyRead.associate = (models) => {
     AlreadyRead.belongsToMany(models.User, { foreignKey: 'userId', as: 'user' });
+    models.User.belongsToMany(models.AlreadyRead, { foreignKey: 'userId', as: 'user' });
     AlreadyRead.belongsToMany(models.Books, { foreignKey: 'bookId', as: 'books' });
   };
   return AlreadyRead;
