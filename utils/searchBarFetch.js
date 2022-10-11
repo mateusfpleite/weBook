05b20fca09032e0da06aa475787a22fbd91e20be
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-11 19:22:27 -0300

    fix: some wrong/obsolete code

--- utils/searchBarFetch.js
+++ utils/searchBarFetch.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const searchBarFetch = async ({value, category=null, maxResults=null, language=null, author=null}) => {
   const lang = language ? `&langRestrict=${language}` : '';
-  const hasMaxResults = maxResults ? `&maxResults=$${maxResults}` : '&maxResults=20';
-  const withCat = category ? `+subject:'${category}'`: '&';
-  const withAuthor = author ? `+inauthor:'${author}'` : '&'
+  const hasMaxResults = maxResults ? `&maxResults=${maxResults}` : '&maxResults=20';
+  const withCat = category ? `+subject:'${category}'`: '';
+  const withAuthor = author ? `+inauthor:'${author}'` : ''
   const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}${withAuthor}${withCat}${hasMaxResults}${lang}`;
   const {
     data: { items },
