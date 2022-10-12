be80456611844e8fa4d9fa8c23cbde7cb9340c92
Mateus <mateusfpleite@gmail.com>
2022-10-12 17:41:55 -0300

    refact: substitute div for fragment

--- components/SearchBar/SearchBarOptions.jsx
+++ components/SearchBar/SearchBarOptions.jsx
@@ -5,14 +5,14 @@ function SearchBarOptions({genericState: {language}, setGenericState}) {
   // console.log(genericState);
   // const {language} = genericState;
   return (
-    <div>
+    <>
       <select name="language" value={language} onChange={setGenericState}>
         <option value={false}>Language (optional)</option>
         {languages.map((lang) => (
           <option key={lang} value={lang}>{isoConv(lang)}</option>
         ))}
       </select>
-    </div>
+    </>
   );
 }
 
