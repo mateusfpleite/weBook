233608de5312a4a75845ed3b73d9dcce32df3b10
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-13 18:56:13 -0300

    fix: the 'favorite books' associations now is working correctly

--- db/models/FavoriteBooks.model.js
+++ db/models/FavoriteBooks.model.js
@@ -23,8 +23,22 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
   FavoriteBooks.associate = (models) => {
-    FavoriteBooks.belongsToMany(models.User, { foreignKey: 'userId', as: 'user' });
-    FavoriteBooks.belongsToMany(models.Books, { foreignKey: 'bookId', as: 'books' });
+    FavoriteBooks.belongsToMany(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+      through: 'Users',
+    });
+    models.Users.belongsToMany(models.FavoriteBooks, {
+      foreignKey: 'userId',
+      as: 'user',
+      through: 'Users',
+    });
+
+    FavoriteBooks.belongsTo(models.Books, { foreignKey: 'bookId', as: 'books' });
+    models.Books.hasMany(models.FavoriteBooks, {
+      foreignKey: 'bookId',
+      as: 'books',
+    });
   };
   return FavoriteBooks;
 };
