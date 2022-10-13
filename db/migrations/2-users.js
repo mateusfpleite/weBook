5e6d8797c141a57928904b94cd5dec3fbd67979e
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-13 18:53:07 -0300

    feat: there is an users table

---
+++ db/migrations/2-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        field: 'first_name',
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        field: 'last_name',
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        allowNull: true,
+        defaultValue: null,
+        type: Sequelize.STRING,
+      },
+      profilePicture: {
+        field: 'profile_picture',
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
