a538957b69d778a9141cb51afa262c8bffaf1208
Mateus <mateusfpleite@gmail.com>
2022-10-14 14:51:38 -0300

    feat: creates page my-books

---
+++ pages/my-books.jsx
@@ -0,0 +1,17 @@
+const MyBooks = ({ data }) => {
+    return (
+        <>
+            <div>
+                <h3>Bookshelves</h3>
+                <h4>All</h4>
+                <h4>Currently Reading</h4>
+                <h4>Read</h4>
+                <h4>Want to Read</h4>
+                <h4>Favorite</h4>
+            </div>
+            
+        </>
+    )
+}
+
+export default MyBooks;
