bf72df7d87a60dfe52ae82f612cecf5ea0c4897f
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-14 20:59:06 -0300

    feat: now the login inputs are all componentizeds

---
+++ components/Login/LoginInputs.jsx
@@ -0,0 +1,53 @@
+import { useContext, useState } from "react";
+import context from "../../context/context";
+import verifyEmail from "../../utils/verifyEmail";
+import TextField from '@mui/material/TextField';
+import { InputAdornment, IconButton } from "@material-ui/core";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+function LoginInputs() {
+  const { genericState, setGenericState } = useContext(context);
+  const [ showPassword, setShowPassword ] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  return (
+    <>
+      <TextField
+        id="outlined-basic1"
+        label="Email"
+        variant="outlined"
+        fullWidth
+        name="emailInput"
+        type="text"
+        onChange={setGenericState}
+        value={genericState.emailInput}
+        error={verifyEmail(genericState.emailInput)}
+        helperText={verifyEmail(genericState.emailInput) ? 'Please, type a valid email' : ''}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        fullWidth
+        name="passwordInput"
+        type={showPassword ? "text" : "password"}
+        onChange={setGenericState}
+        value={genericState.passwordInput}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
+      />
+    </>
+  )
+}
+
+export default LoginInputs;
