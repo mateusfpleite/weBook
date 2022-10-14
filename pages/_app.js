01be3d737b7110f6314d51f1b9ff1468ff64a00e
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-14 17:55:40 -0300

    feat: now we are using material UI for styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,37 @@
 import ContextProvider from '../context/ContextProvider';
-import '../styles/globals.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <ContextProvider>
-      <Component {...pageProps} />
-    </ContextProvider>
+    <React.Fragment>
+      <Head>
+        <title>weBook</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <CssBaseline />
+      <ContextProvider>
+        <Component {...pageProps} />
+      </ContextProvider>
+    </React.Fragment>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
