2099027c72ba260dd763ddc98ef8f5b657f7a5bd
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-14 14:05:40 -0300

    formats the document

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -26,45 +26,45 @@ function Home({ fictionBooks, nonFictionBooks }) {
       <h1>You are in the home page!</h1>
       <Header />
       <h2>Month Bestsellers - Fiction</h2>
-      <Carousel  
-  showDots={true}
-  responsive={responsive}
-  ssr={true}
-  infinite={true}
-  autoPlay={false}
-  transitionDuration={500}
-  itemClass="carousel-item-padding-40-px">
-      {fictionBooks.map((book) => (
-        <div key={book.primary_isbn10} style={{ maxWidth: '100%', height: '250px' }}>
-      {/* <div style={{display: 'flex', flexFlow: 'row wrap'}}> */}
-        <BookCard
-        id={book.primary_isbn10}
-        thumbnail={book.book_image}
-        title={book.title}
-        author={book.author}
-        bestSeller={true} />
-        </div>
-      ) )}
+      <Carousel
+        showDots={true}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        autoPlay={false}
+        transitionDuration={500}
+        itemClass="carousel-item-padding-40-px">
+        {fictionBooks.map((book) => (
+          <div key={book.primary_isbn10} style={{ maxWidth: '100%', height: '250px' }}>
+            {/* <div style={{display: 'flex', flexFlow: 'row wrap'}}> */}
+            <BookCard
+              id={book.primary_isbn10}
+              thumbnail={book.book_image}
+              title={book.title}
+              author={book.author}
+              bestSeller={true} />
+          </div>
+        ))}
       </Carousel>
       <h2> Month Bestsellers - Non-Fiction</h2>
-      <Carousel 
-      showDots={true}
-      responsive={responsive}
-      ssr={true}
-      infinite={true}
-      autoPlay={false}
-      transitionDuration={500}
-      itemClass="carousel-item-padding-40-px">
-      {nonFictionBooks.map((book) => (
-        <div key={book.primary_isbn10} style={{ maxWidth: '100%', height: '250px' }}>
-        <BookCard
-        id={book.primary_isbn10}
-        thumbnail={book.book_image}
-        title={book.title}
-        author={book.author}
-        bestSeller={true} />
-        </div>
-      ) )}
+      <Carousel
+        showDots={true}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        autoPlay={false}
+        transitionDuration={500}
+        itemClass="carousel-item-padding-40-px">
+        {nonFictionBooks.map((book) => (
+          <div key={book.primary_isbn10} style={{ maxWidth: '100%', height: '250px' }}>
+            <BookCard
+              id={book.primary_isbn10}
+              thumbnail={book.book_image}
+              title={book.title}
+              author={book.author}
+              bestSeller={true} />
+          </div>
+        ))}
       </Carousel>
       {/* </div> */}
       {/* <FacebookLogin
@@ -82,8 +82,8 @@ export default Home;
 export async function getStaticProps() {
   const fictionURL = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=Rgr3HfLslGy1GfH6kyKNNIuqrAO5G0Ij';
   const nonFictionURL = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?api-key=Rgr3HfLslGy1GfH6kyKNNIuqrAO5G0Ij'
-  const {data: {results: {books: fictionBooks }}} = await axios.get(fictionURL);
-  const {data: {results: {books: nonFictionBooks }}} = await axios.get(nonFictionURL);
+  const { data: { results: { books: fictionBooks } } } = await axios.get(fictionURL);
+  const { data: { results: { books: nonFictionBooks } } } = await axios.get(nonFictionURL);
 
   return {
     props: {
