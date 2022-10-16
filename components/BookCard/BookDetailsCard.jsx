052ad70871751a80828225817d3210d598693ad0
Mateus <mateusfpleite@gmail.com>
2022-10-16 20:06:08 -0300

    feat: working on inserting and updating users_books

--- components/BookCard/BookDetailsCard.jsx
+++ components/BookCard/BookDetailsCard.jsx
@@ -1,13 +1,56 @@
-import { FormControl, MenuItem, Select } from '@mui/material';
+import { FormControlLabel, FormGroup, Switch } from '@mui/material';
 import isoConv from 'iso-language-converter';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import supabase from '../../utils/supabaseClient';
 
-const shelves = ['favorite_books', 'is_reading', 'want_to_read', 'already_read' ];
-
-
 function BookDetailsCard({ book }) {
-  const [selectedShelf, setShelf] = useState('want_to_read');
+  const [shelves, setShelves] = useState({
+    wantToRead: false,
+    reading: false,
+    read: false,
+  });
+const [selectedShelf, selectShelf] = useState('');
+const [isFavorite, setFavorite] = useState(false);
+  useEffect(() => {
+   async function updateShelves() {
+      const loggedUser = Number(JSON.parse(localStorage.getItem('loggedUser'))) || 0;
+      const { data } = await supabase.from('books').select('*').eq('book_id', book.id);
+      if (!(data.length)) {
+        await supabase
+        .from('books')
+        .insert([
+          { book_id: book.id, title, book_picture: bookImg },
+        ]);
+      }
+      if (selectedShelf) {
+        const {data: check} = await supabase.from('users_books').select('*').match({book_id: book.id, user_id: loggedUser});
+        if (!(check.length)) {
+          await supabase
+          .from('users_books')
+          .insert([
+            { book_id: book.id, user_id: loggedUser, status: selectedShelf, favorite: isFavorite },
+          ]);
+        } else {
+          await supabase
+          .from('users_books')
+          .update({ status: selectedShelf, favorite: isFavorite })
+          .match({book_id: book.id, user_id: loggedUser});
+        }
+      } else {
+        await supabase
+        .from('users_books')
+        .update({favorite: isFavorite })
+        .match({book_id: book.id, user_id: loggedUser});
+      }
+    }
+    updateShelves();
+  }, [selectedShelf, isFavorite])
+
+  useEffect (() => {
+    async function updateFavorites() {
+      
+    }
+  })
   const {
     pageCount,
     authors,
@@ -24,45 +67,82 @@ function BookDetailsCard({ book }) {
 
   const bookImg = imageLinks?.thumbnail || 'https://tinyurl.com/36ys7v4w';
 
-  const favoriteBook = async ({target}) => {
-  const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
-  await supabase
-    .from('books')
-    .insert([
-      { book_id: book.id, title, book_picture: bookImg },
-    ]);
-     await supabase
-    .from(target.value)
-    .insert([
-      { book_id: book.id, user_id: +loggedUser },
-    ]);
+  const favoriteBook = async ({ target }) => {
+    await supabase
+      .from(target.value)
+      .insert([
+        { book_id: book.id, user_id: +loggedUser },
+      ]);
   }
 
- 
+  const handleChange = async ({ target }) => {
+      setShelves({...shelves, [target.name]: !(shelves[target.name])});
+      if (target.name === 'reading') {
+        setShelves({...shelves, wantToRead: false, read: false, reading: !(shelves.reading) });
+      }
+      if (target.name === 'wantToRead') {
+        setShelves({...shelves, reading: false, read: false, wantToRead: !(shelves.wantToRead) });
+      }
+      if (target.name === 'read') {
+        setShelves({...shelves, reading: false, wantToRead: false, read: !(shelves.read) });
+      }
+      if (target.checked) selectShelf(target.name);
+  };
+
   return (
     <div>
       <h1>{title}</h1>
       <h3>{subTitle}</h3>
       <h3>Authors: {authors?.join(', ') || "No author specified"}</h3>
       <h3>Language: {isoConv(language)}</h3>
-      <img 
-      style={{ maxWidth: '128px', height: '170px' }}
-      src={bookImg}
-       alt="" />
+      <img
+        style={{ maxWidth: '128px', height: '170px' }}
+        src={bookImg}
+        alt="" />
       <div>
-        { description ? <p dangerouslySetInnerHTML={{__html: description }} /> : <p>No description avaliable</p> }
+        {description ? <p dangerouslySetInnerHTML={{ __html: description }} /> : <p>No description avaliable</p>}
 
-        <FormControl fullWidth>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-        </Select>
-      </FormControl>
-        <button type="button" onClick= {favoriteBook}>Favorite book</button>
+        <FormGroup row>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={isFavorite}
+                onChange={() => setFavorite(!isFavorite)}
+              />
+            }
+            label="Favorite book"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                name='wantToRead'
+                checked={shelves.wantToRead}
+                onChange={handleChange}
+              />
+            }
+            label="Want to Read"
+          />
+            <FormControlLabel
+            control={
+              <Switch
+                name='reading'
+                checked={shelves.reading}
+                onChange={handleChange}
+              />
+            }
+            label="Reading"
+          />
+            <FormControlLabel
+            control={
+              <Switch
+                name='read'
+                checked={shelves.read}
+                onChange={handleChange}
+              />
+            }
+            label="Read"
+          />
+        </FormGroup>
         <p>Number of pages: {pageCount}</p>
         <p>Published: {publishedDate}</p>
         <p>Publisher: {publisher}</p>
