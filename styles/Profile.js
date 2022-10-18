98b20145bf6966640a651dccc903ab12e7486885
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-18 02:20:21 -0300

    feat: there is some styled material components for profile page

---
+++ styles/Profile.js
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { CustomStack } from '../styles/Login';
+
+
+export const NewCustomStack = styled(CustomStack)`
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const CustomTextField = styled(TextField)`
+  width: 100%;
+`;
