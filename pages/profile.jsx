284f71598d1f0cb612a06d800fe015eeef798081
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-18 02:19:33 -0300

    the profile page is almost completed

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,35 +1,20 @@
 import { useState } from 'react';
-import useGenericState from '../hooks/useGenericState';
-import TextField from '@mui/material/TextField';
 import 'react-next-dates/dist/style.css';
 import Button from '@mui/material/Button';
 import { CustomForm, CustomPaper, CustomParagraph } from '../styles/Login';
 import { useEffect } from 'react';
 import jwt from 'jsonwebtoken';
 import supabase from '../utils/supabaseClient';
+import { CustomTextField, NewCustomStack } from '../styles/Profile';
+import ProfileImg from '../components/Profile/ProfileImg';
+import { TextField } from '@mui/material';
 
 const JWT_SECRET = process.env.NEXT_PUBLIC_DB_PASSWORD;
 
 function Profile() {
-  const [ profilePic, setProfilePic ] = useState();
-  const [ willChangePic, setWillChangePic ] = useState(false);
   const [ editProfile, setEditProfile ] = useState(false);
   const [ userProfileInfos, setUserProfileInfos ] = useState({});
-  // const [ startDate, setStartDate ] = useState(new Date());
-  const picture = profilePic || 'https://i.imgur.com/r0t9T9j.png';
-
-  const INITIAL_STATE = { profileDesc: '' };
-
-  const [ genericState, setGenericState ] = useGenericState(INITIAL_STATE);
-
-  const { profileDesc } = genericState;
-
-  const changeProfilePic = (files) => {
-    const file = files[ 0 ];
-    const profilePic = URL.createObjectURL(file);
-    setProfilePic(profilePic);
-    setWillChangePic(false);
-  }
+  const [ profileDesc, setProfileDesc ] = useState('');
 
   useEffect(() => {
     const userToken = JSON.parse(localStorage.getItem('loggedUser'));
@@ -40,76 +25,86 @@ function Profile() {
         .select('*')
         .eq('id', id);
       setUserProfileInfos(data[ 0 ])
-      console.log(data)
+      console.log(data[ 0 ])
+      if (data[ 0 ].description) setProfileDesc(data[ 0 ].description);
     }
     fetchUserInfos();
-  }, [])
+  }, []);
 
+  const saveUserInfos = async (e) => {
+    e.preventDefault();
+    setEditProfile(!editProfile)
+    await supabase
+      .from('users')
+      .update(
+        {
+          first_name: 'Leonardo'
+        }
+      )
+      .eq('id', userProfileInfos.id)
+  }
 
   return (
-    <CustomForm method='POST'>
+    <CustomForm onSubmit={saveUserInfos}>
       <CustomPaper>
-        <img
-          src={userProfileInfos.profile_picture || picture}
-          alt=''
-          style={{ width: '80px', borderRadius: '100%', border: '1px solid black' }}
-        />
+        <NewCustomStack spacing={3}>
+          <ProfileImg userProfileInfos={userProfileInfos} editProfile={editProfile} />
 
-        <CustomParagraph>
-          {`${userProfileInfos.first_name} ${userProfileInfos.last_name}`}
-        </CustomParagraph>
+          {!editProfile
+            ? (
+              <>
+                <CustomParagraph>
+                  {`${userProfileInfos.first_name} ${userProfileInfos.last_name}`}
+                </CustomParagraph>
 
-        <CustomParagraph>
-          {userProfileInfos.email}
-        </CustomParagraph>
-        {editProfile &&
-          <>
-            {!willChangePic ?
-              (
-                <button
-                  type='button'
-                  onClick={() => setWillChangePic(true)}
+                <CustomParagraph>{userProfileInfos.email}</CustomParagraph>
+              </>
+            )
+            : (
+              <>
+                <TextField
+                  label="Name:"
+                  name="first_name"
                 >
-                  Change profile picture
-                </button>
-              )
-              : (
-                <input
-                  type='file'
-                  onChange={({ target: { files } }) => changeProfilePic(files)}
-                />
-              )
-            }
-          </>
-        }
+                  opa
+                </TextField>
 
-        {editProfile &&
-          <Button
-            variant='contained'
-            type='button'
-            fullWidth
-          >
-            Change description
+                <TextField
+                  label="Email:"
+                >
+                  opa
+                </TextField>
+              </>
+            )
+          }
+
+          {!editProfile
+            ? (
+              <CustomParagraph>
+                {userProfileInfos.description
+                  ? userProfileInfos.description
+                  : "It seems you haven't put any description yet :("
+                }
+              </CustomParagraph>
+            )
+            :
+            (
+              <CustomTextField
+                name='profileDesc'
+                multiline
+                inputProps={{ maxLength: 160 }}
+                helperText={`${profileDesc.length}/160`}
+                value={profileDesc}
+                label="Description: (type something cool about you :D )"
+                onChange={({ target: { value } }) => setProfileDesc(value)}
+              />
+            )
+          }
+
+          <Button fullWidth type='submit' variant='contained'>
+            {!editProfile ? 'Edit profile' : 'Save'}
           </Button>
-        }
-
-        <TextField
-          name='profileDesc'
-          multiline
-          inputProps={{ maxLength: 160 }}
-          helperText={`${profileDesc.length}/160`}
-          value={profileDesc}
-          onChange={setGenericState}
-        />
-
-        <Button
-          onClick={() => setEditProfile(true)}
-          fullWidth
-          type='button'
-          variant='contained'
-        >
-          { !editProfile ? 'Edit profile' : 'Save' }
-        </Button>
+        </NewCustomStack>
       </CustomPaper>
     </CustomForm >
   );
