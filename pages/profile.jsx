1d6368aaeb867d22e1d78300e78782c04fe9b6dc
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-17 21:11:57 -0300

    feat: now the profile page is fetching the profile infos from the right user id

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 import useGenericState from '../hooks/useGenericState';
 import TextField from '@mui/material/TextField';
 import 'react-next-dates/dist/style.css';
+import Button from '@mui/material/Button';
+import { CustomForm, CustomPaper } from '../styles/Login';
+import { useEffect } from 'react';
+import jwt from 'jsonwebtoken';
+import supabase from '../utils/supabaseClient';
 // import { ptBR } from 'date-fns/locale';
 // import { DatePicker } from 'react-next-dates';
+
+const JWT_SECRET = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 function Profile() {
   const [ profilePic, setProfilePic ] = useState();
   const [ willChangePic, setWillChangePic ] = useState(false);
+  const [ editProfile, setEditProfile ] = useState(false);
+  const [ userProfileInfos, setUserProfileInfos ] = useState({});
   // const [ startDate, setStartDate ] = useState(new Date());
-  const picture = profilePic || "https://i.imgur.com/r0t9T9j.png";
+  const picture = profilePic || 'https://i.imgur.com/r0t9T9j.png';
 
   const INITIAL_STATE = { profileDesc: '' };
 
@@ -32,54 +41,85 @@ function Profile() {
     setWillChangePic(false);
   }
 
+  useEffect(() => {
+    const userToken = JSON.parse(localStorage.getItem('loggedUser'));
+    const { data: { id } } = jwt.verify(userToken, JWT_SECRET)
+    const fetchUserInfos = async () => {
+      const { data } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', id);
+      setUserProfileInfos(data[0])
+    }
+    fetchUserInfos();
+  }, [])
+
+
   return (
-    <form method="POST">
-      <h1>Profile Page</h1>
-      <img
-        src={picture}
-        alt=""
-        style={{ width: '80px', borderRadius: '100%', border: '1px solid black' }}
-      />
-      <br />
+    <CustomForm method='POST'>
+      <CustomPaper>
+        <img
+          src={picture}
+          alt=''
+          style={{ width: '80px', borderRadius: '100%', border: '1px solid black' }}
+        />
+        <br />
 
-      {!willChangePic &&
-        (<button
-          type="button"
-          onClick={() => setWillChangePic(true)}
-        >
-          Change profile picture
-        </button>)
-      }
+        {editProfile &&
+          <span>
+            {!willChangePic ?
+              (
+                <button
+                  type='button'
+                  onClick={() => setWillChangePic(true)}
+                >
+                  Change profile picture
+                </button>
+              )
+              : (
+                <input
+                  type='file'
+                  onChange={({ target: { files } }) => changeProfilePic(files)}
+                />
+              )
+            }
+          </span>
+        }
 
-      {willChangePic &&
-        (<input
-          type="file"
-          onChange={({ target: { files } }) => changeProfilePic(files)}
-        />)
-      }
-      {/* <label>
-        Birthdate
-        <DatePicker locale={ptBR} date={startDate} onChange={setStartDate}>
-          {({
-      inputProps
-    }) => <input {...inputProps} />}
-        </DatePicker> */}
-      {/* </label> */}
-      <TextField
-        name="profileDesc"
-        multiline
-        inputProps={{ maxLength: 160 }}
-        helperText={`${profileDesc.length}/160`}
-        value={profileDesc}
-        onChange={setGenericState}
-      />
+        {editProfile &&
+          <Button
+            variant='contained'
+            type='button'
+            fullWidth
+          >
+            Change description
+          </Button>
+        }
 
+        <TextField
+          name='profileDesc'
+          multiline
+          inputProps={{ maxLength: 160 }}
+          helperText={`${profileDesc.length}/160`}
+          value={profileDesc}
+          onChange={setGenericState}
+        />
+        {/* 
       <p
         style={{ color: textAreaLengthColor(profileDesc), transition: '1s' }}
-      >
+        >
+      </p> */}
 
-      </p>
-    </form >
+        <Button
+          onClick={() => setEditProfile(true)}
+          fullWidth
+          type='button'
+          variant='contained'
+        >
+          Edit profile
+        </Button>
+      </CustomPaper>
+    </CustomForm >
   );
 }
 
